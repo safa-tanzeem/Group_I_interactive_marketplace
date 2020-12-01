14d9ad83246ccc40c19b3c703e1190bd48f726f2
Muhammad Salman <muhammads09@hotmail.com>
2020-11-30 23:28:40 -0500

    checkout() function started

--- buyer_functions.h
+++ buyer_functions.h
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "file_scanner.h"
 #include "product.h"
+#include <unistd.h>
+#include <io.h>
 
 int display_categories(){
 	
@@ -70,64 +72,9 @@ struct PRODUCT* display_product(){
 
     return new_product_head;
     
-    /*
-    struct PRODUCT *user_list; 
-	user_list = (struct PRODUCT*)malloc(sizeof(struct PRODUCT));
-    
-    struct PRODUCT *list_products; 
-	list_products = (struct PRODUCT*)malloc(sizeof(struct PRODUCT));
-
-    user_list = list_products;
-    
-    while(1){
-	
-    	printf("\nSelect products to add to cart. Enter product numbers.\nEnter 0 to end entries: \n");
-    	scanf("%i",&product_sel);
-	    
-	    if(product_sel==0){
-	    	break;
-		}
-	    
-	    struct PRODUCT *tracker; 
-		tracker = (struct PRODUCT*)malloc(sizeof(struct PRODUCT));
-		
-		tracker = new_product_head;
-		 
-		while(tracker!=NULL){
-
-			int selected = 0;
-			if(tracker->product_number==product_sel){
-		        
-				struct PRODUCT *product; 
-		        product = (struct PRODUCT*)malloc(sizeof(struct PRODUCT));
-	    
-	            product=tracker;
-	        
-	            printf("You added %s to your cart.\n", tracker->name);
-	            printf("How many would you like? ");
-	            scanf("%i",&selected);
-	            product->number_selected = selected;
-			    
-	            list_products->next_product = product;
-	            list_products = list_products->next_product;
-	            
-	            break;
-		        
-			}
-			
-	        tracker=tracker->next_product;
-	    }
-	}
-    
-    list_products->next_product = NULL;
-    user_list = user_list->next_product;
-    
-    return user_list;
-    */
-    
 }
 
-void add_cart(){
+int add_cart(){
 	
 	int product_sel;
 	    
@@ -180,7 +127,7 @@ void add_cart(){
 					printf("How many would you like? You will be re-prompted if we do not enough stock: ");
 	            	scanf("%i",&selected);
 	            
-				}while(selected>tracker->stock);
+				}while(selected>tracker->stock || selected ==0);
 	            
 	            product->number_selected = selected;
 			    
@@ -198,7 +145,107 @@ void add_cart(){
     list_products->next_product = NULL;
     user_list = user_list->next_product;
     
-    return user_list;
+    tracker = user_list;
     
+    int dis_code=100;
+    
+    while(1){
+	
+    	printf("\nPlease enter any discount codes you would like to enter: Enter 0 to exit: ");
+    	scanf("%i", &dis_code);
+		
+		if(dis_code==0){
+			break;
+		}
+		
+		while(tracker!=NULL){
+
+			if(tracker->discount_code==dis_code){
+		        
+		        if(tracker->flag_code != 1){
+				
+		        	tracker->price = (1-(tracker->discount_percentage))*(tracker->price);
+	            	printf("\nYou applied a discount code to %s.\n", tracker->name);
+             		printf("You're new price per product is %.2f.\n", tracker->price);
+                	tracker->flag_code = 1;
+	            	break;
+		    	}
+		    	else{
+		    		printf("You have already applied a discount to this product!\n");
+				}
+			}
+			
+			tracker=tracker->next_product;
+		}
+	}
+	
+	user_list=tracker;
+	
+	checkout(user_list);
+	return 0;
+	
 }
 
+int receipt_exists(const char *receipt_name){
+	
+	
+	strcat(receipt_name, ".txt");
+	FILE *receipt = fopen(receipt_name, "r");
+
+	if (receipt == NULL){
+		return 0;
+	}
+	fclose(receipt);
+
+	return 1;
+}
+
+int checkout(struct PRODUCT buyer_products){
+	
+	struct PRODUCT *tracker; 
+	tracker = (struct PRODUCT*)malloc(sizeof(struct PRODUCT));
+	tracker = buyer_products;
+    
+	int counter = 1;
+	char counter_str [10];
+    char receipt_name [20];
+    char receipt_def [20];
+    
+    strcpy(receipt_name,"receipt");
+    
+    
+	while (1){
+		
+		if(receipt_exists(receipt_name)==0)
+		{
+			printf("NULL");
+			break;
+		}
+		
+		strcpy(receipt_def,"receipt");
+		
+		itoa(counter, counter_str, 10);
+
+		strcat(receipt_def, counter_str);
+	
+		strcpy(receipt_name, receipt_def);
+
+		counter++;
+
+ 	}
+ 	
+ 	FILE *output; 
+    output = fopen(receipt_name, "w+");
+    fprintf(output, "Thank you for your purchase today!\n--------------------------------------------------------------------\n");
+    
+	while(tracker->next_product!=NULL){
+    	fprintf(output, "Thank you for your purchase today!\n--------------------------------------------------------------------\n");
+    	
+    	
+	}
+    fclose(output);
+    printf("\nThank you for shopping with us! Your receipt has been outputted.\n\n");
+    
+	return 0;
+	
+}
