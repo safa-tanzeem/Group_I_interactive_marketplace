530f026d3a5649518e706fa70db8e5b34bb717a3
mdsufianali <samer.sami086@gmail.com>
2020-12-16 22:08:28 -0500

    inventory info displaying

---
+++ display_inventory.h
@@ -0,0 +1,13 @@
+/*
+ * display_inventory.h
+ *
+ *  Created on: Dec. 6, 2020
+ *      Author: sufian
+ */
+
+#ifndef DISPLAY_INVENTORY_H_
+#define DISPLAY_INVENTORY_H_
+
+int display_inventory(seller_t *seller);
+
+#endif /* DISPLAY_INVENTORY_H_ */
