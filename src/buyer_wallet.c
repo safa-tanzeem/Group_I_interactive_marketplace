eb774223278be28f566a836377da6cd2d472e5ee
Safa Tanzeem <65215770+safa-tanzeem@users.noreply.github.com>
2020-12-19 09:20:02 -0500

    Update buyer_wallet.c

--- src/buyer_wallet.c
+++ src/buyer_wallet.c
@@ -18,7 +18,7 @@
  * @return  balance added in the one wallet of the buyer
  */
 float add_balance(){
-    int output;
+    int output = 0;
     float balance = 0.0;
     printf("Declare the amount you have in CAD: ");
 
@@ -39,11 +39,12 @@ float add_balance(){
  * @return update_balance if insufficient balance in the account and user to want to add new balance
  */
 float update_balance(float wallet_balance, float total_amt_in_cart){
-    int output;
+    int output = 0;
     int flag = 0;
     if (wallet_balance < total_amt_in_cart){
         printf("Transaction cannot be completed, Wallet Balance insufficient! \n");
         printf("Enter 1: To add balance \nEnter 0: To exit \n");
+        
         do{
             output=scanf("%d", &flag);
         }while(validate(output)!=OK);
