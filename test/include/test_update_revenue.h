5c9504b5f425cc78bcb9ecebe744e369e14c9a79
Kaveh-Rouhandeh <73255465+Kaveh-Rouhandeh@users.noreply.github.com>
2020-12-20 17:58:56 -0500

    Add files via upload

---
+++ test/include/test_update_revenue.h
@@ -0,0 +1,6 @@
+#ifndef TEST_UPDATE_REVENUE_H_INCLUDED
+#define TEST_UPDATE_REVENUE_H_INCLUDED
+
+#define PATH                                            "../log/log_update_revenue.log"
+
+#endif // TEST_UPDATE_REVENUE_H_INCLUDED
