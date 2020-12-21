c56154c2397fa14dbfd76c543b15791f733b7db1
mdsufianali <samer.sami086@gmail.com>
2020-12-20 19:44:21 -0500

    adding r2 with test

---
+++ include/display_sold_products.h
@@ -0,0 +1,13 @@
+/*
+ * display_sold_products.h
+ *
+ *  Created on: Dec. 13, 2020
+ *      Author: sufian
+ */
+
+#ifndef DISPLAY_SOLD_PRODCUTS_H_
+#define DISPLAY_SOLD_PRODUCTS_H_
+
+int display_sold_products(struct SELLER *seller);
+
+#endif /* DISPLAY_SOLD_PRODUCTS_H_ */
